c0f363bb8917bfa0548024e4ee727e34e724f850
Christopher Fryman <farfromguam@gmail.com>
2013-02-19 15:56:00 -0600

    bad habbits, days work

--- js/morejs.js
+++ js/morejs.js
@@ -1,6 +1,11 @@
 function manage() {
-    alert("You are now logged in");
+    console.log("You are now logged in");
     $(".manage").css("display", "inline");
+}
+
+function startSeason() {
+    $(".playing").css("display", "none");
+    $(".standings").css("display", "inline");
 }
 
 //define Variables
@@ -19,13 +24,35 @@ function Team (teamName, mgrFirst, mgrLast, mgrPhone, mgrEmail, mgrZip, sponsor,
     this.wins = wins;
     this.losses = losses;
     var stat = wins/(wins+losses);
+        //if (stat === NaN) (this.stat = 0);
     this.percent = stat.toFixed(3);
+    this.rank = stat * 100;
     }
 
 // Now we can make an array of people
 var league = new Array();
-league[0] = new Team("Ardvarks", "Christopher", "Fryman", 9016045976, "farfromguam@gmail.com", 37210, "Fryman and Assoiciates", 67, 17);
-league[1] = new Team("Boss Hoggs", "Joe", "Shepherd", 6154840875, "shepright@comcast.net", 37205, "Brewhouse West", 3, 2);
+league[0] = new Team("Ardvarks", "Christopher", "Fryman", 9016045976, "farfromguam@gmail.com", 37210, "Fryman and Assoiciates", 10, 10);
+league[1] = new Team("Boss Hoggs", "Joe", "Shepherd", 6154840875, "shepright@comcast.net", 37205, "Brewhouse West", 4, 1);
+//league[2] = new Team("Cozy Bros", "Joe", "Shepherd", 6154840875, "shepright@comcast.net", 37205, "Snuggie corp of America", 6, 10);
+//league[3] = new Team("Dudly Doo Doo", "Joe", "Shepherd", 6154840875, "shepright@comcast.net", 37205, "Snuggie corp of America", 43, 2);
+//league[4] = new Team("Every Other Thing", "Joe", "Shepherd", 6154840875, "shepright@comcast.net", 37205, "Snuggie corp of America", 5, 1);
+//league[5] = new Team("Foo Bar", "Joe", "Shepherd", 6154840875, "shepright@comcast.net", 37205, "Snuggie corp of America", 6, 6);
+//league[6] = new Team("Gangnam Style", "Joe", "Shepherd", 6154840875, "shepright@comcast.net", 37205, "Snuggie corp of America", 3, 2);
+//league[7] = new Team("Herion Junkies", "Joe", "Shepherd", 6154840875, "shepright@comcast.net", 37205, "Snuggie corp of America", 1, 0);
+
+
+
+// reusable sort functions, and sort by any field
+// http://stackoverflow.com/questions/979256/how-to-sort-an-array-of-javascript-objects
+var sort_by = function(field, reverse, primer){
+   var key = primer ? function (x) { return primer(x[field]); } : function (x) { return x[field]; }
+   return function (a,b) {
+       var A = key(a), B = key(b);
+       return (A < B ? -1 : (A > B ? 1 : 0)) * [1,-1][+!!reverse];                  
+   }
+}
+
+
 
 
 // Joes modal stuff
