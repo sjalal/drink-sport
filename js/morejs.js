c3b9c7b26c83a2589836918ddbe152e2d87165b8
Christopher Fryman <farfromguam@gmail.com>
2013-02-22 14:08:26 -0600

    setting up for backdraft

--- js/morejs.js
+++ js/morejs.js
@@ -1,67 +1,120 @@
-// $(document).ready(function () {
-//     // get from database
-//     $.ajax({
-//         url: 'json/drink-sport.json',
-//         type: "GET",
-//         dataType: "jsonp",
-//         success: function(data) {
-//             alert(data);
-//         },
+$(document).ready(function () {
+  // get from database
+  $.ajax({
+    url: 'backliftapp/team',
+    type: "GET",
+    dataType: "json",
+    success: function (data) {
+      leagueAll = data;
+      for (var i = 0; i < data.length; i++) {
+        addTeamToTable(data[i]);
+        populateTeamList(data[i]);
+      }
+      doPopovers();
+      track("Json init!");
+      track("Teams loaded!");
+    }
+  }); // end ajax
+}); // end ready
 
-//     }) // end ajax
-// }); // end ready
+// // The league 
+// // 0-team name, 1-Manager, 2-Manager Last, 3-Phone, 4-Email, 5-Zip, 6-Sponsor, 7-Wins, 8-Losses, 9-Percent
+// var league2d = [
+//     ["Ardvarks", "Christopher", "Fryman", "9016045976", "farfromguam@gmail.com", "37210", "Fryman and Assoiciates", "W", "L", "%"],
+//     ["Boss Hoggs", "Joe", "Shepherd", "6154840875", "shepright@comcast.net", "37205", "Brewhouse West", "W", "L", "%"]
+// ]
 
-// The league 
-// 0-team name, 1-Manager, 2-Manager Last, 3-Phone, 4-Email, 5-Zip, 6-Sponsor, 7-Wins, 8-Losses, 9-Percent
-var league2d = [
-    ["Ardvarks", "Christopher", "Fryman", "9016045976", "farfromguam@gmail.com", "37210", "Fryman and Assoiciates", "W", "L", "%"],
-    ["Boss Hoggs", "Joe", "Shepherd", "6154840875", "shepright@comcast.net", "37205", "Brewhouse West", "W", "L", "%"]
-]
+var leagueAll = [];
 
 // add team to league
-    function addTeam() {
-        league2d.push(
-        [
-        $("#inputTeamName").val(),
-        $("#inputMgrFirst").val(),
-        $("#inputMgrLast").val(),
-        $("#inputMgrPhone").val(),
-        $("#inputMgrEmail").val(),
-        $("#inputSponsor").val(),
-            "W",
-            "L",
-            "%", ]);
-        alert("Thank you, " + $("#inputMgrFirst").val() + ', For registering your team "' + $("#inputTeamName").val() + '"');
-        clearForm();
-        console.log(JSON.stringify(league2d));
-    }; // end add team
+function addTeam() {
+
+  var team = {
+    name: $("#inputTeamName").val(),
+    mgrFirst: $("#inputMgrFirst").val(),
+    mgrLast: $("#inputMgrLast").val(),
+    mgrPhone: $("#inputMgrPhone").val(),
+    mgrEmail: $("#inputMgrEmail").val(),
+    mgrZip: $("inputMgrZip").val(),
+    sponsor: $("#inputSponsor").val(),
+    wins: 0,
+    losses: 0
+  };
+
+  $.ajax({
+    url: 'backliftapp/team',
+    type: "POST",
+    dataType: "json",
+    data: team,
+    success: function (data) {
+      console.dir(data);
+      `
+    }
+  });
+
+}; // end add team
+
+$(".deleteTeam").click(function () {
+  alert(this.data.id);
+});
+
+function track(item) {
+  $('#console').append(item + "<br>");
+}
 
 function clearForm() {
-    $(".teamImput").each(function () {
-        $(this).val("");
-    });
+  $(".teamImput").each(function () {
+    $(this).val("");
+  });
 };
 
 function manage() {
-    console.log("You are now logged in");
-    $(".manage").css("display", "inline");
+  track("You are now logged in");
+  $(".manage").css("display", "inline");
 }
 
 function startSeason() {
-    $(".playing").css("display", "none");
-    $(".standings").css("display", "inline");
+  $(".playing").css("display", "none");
+  $(".standings").css("display", "inline");
+  track("Season Started!")
+}
+
+function populateTeamList(team) {
+  $(
+    "<h4 class='show'>" + team.name + " <em>sponsored by</em> " + team.sponsor + " </h4>" + 
+    "<p class='more'>" +
+    "Manager: " + team.mgrFirst + " " + team.mgrLast + "<br>" +
+    "Phone: " + team.mgrPhone + "<br>" +
+    "E-mail: " + team.mgrEmail + "<br>" +
+    "<a class='manage deleteTeam'>Delete Team</a>" +
+    "</p>").appendTo('#teamList');
+}
+
+function addTeamToTable(team) {
+  $(
+    "<tr>" +
+    "<td>" + team.id + "</td>" +
+    "<td><span class='show'>" + team.name + " <em>sponsored by</em> " + team.sponsor + "</span>" +
+    "<p class='more'><br>" +
+    "Manager: " + team.mgrFirst + " " + team.mgrLast + "<br>" +
+    "Phone: " + team.mgrPhone + "<br>" +
+    "E-mail: " + team.mgrEmail + "<br></p>" +
+    "</td>" +
+    "<td>" + team.wins + "</td>" +
+    "<td>" + team.losses + "</td>" +
+    "<td>" + "calculate %" + "</td>" +
+    "</tr>").appendTo('#teamStandings tbody');
 }
 
 // Popover like functionality 
-$(document).ready(function () {
-    $('.more').hide();
-    $('.show').click(
-
-    function () {
-        $(this).next('.more').toggle();
-    });
-}); // end ready
-
+function doPopovers() { 
+  $('.more').hide();
+  $('.show').click(
+  function () {
+    $(this).next('.more').toggle();
+    track("Popover working!")
+  });
+}
 
 // // reusable sort functions, and sort by any field
 // // http://stackoverflow.com/questions/979256/how-to-sort-an-array-of-javascript-objects
