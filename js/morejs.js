1056cff41147fcf672240aaefcead84c40d4186b
Joe Shepherd <joeshepmedia@gmail.com>
2013-02-25 15:23:13 -0600

    Add working submit and delete code

--- js/morejs.js
+++ js/morejs.js
@@ -1,28 +1,30 @@
-var leagueAll = [];
-
 $(document).ready(function () {
+  track("Document Ready");
   getFromDatabase();  
 }); // end ready
 
-function getFromDatabase() {
-  
+function getFromDatabase() {  
+
   $('#teamList').empty();
   $('#teamTable tbody').empty();
-  track("Cleared Old Data!")
+  $('#signUpButton').empty();
+  $('#gameSchedules').empty();
+  track("Cleared old data")
 
   $.ajax({
     url: 'backliftapp/team',
     type: "GET",
     dataType: "json",
     success: function (data) {
-      leagueAll = data;
+      track("Connected to database");
+      displayButton(data.length);
+      populateGameSchedules(data.length);
       for (var i = 0; i < data.length; i++) {
         populateTeamTable(data[i]);
         populateTeamList(data[i]);
       }
       doPopovers();
-      track("Connected to database!");
-      track("Team Data loaded!");
+      track("Fresh data loaded");
     }
   }); // end ajax
 };
@@ -104,6 +106,16 @@ function displayButton() {
     ).appendTo('#signUpButton');
 }
 
+function displayButton(x) {
+  if (x < 4) {
+    $('#signUpButton').append('<a href="#myModal" role="button" class="btn btn-warning" data-toggle="modal">We need you to sign up!</a>');
+  } else if (x >= 4 && x < 8) {
+    $('#signUpButton').append('<a href="#myModal" role="button" class="btn" data-toggle="modal">Sign up your team today!</a>');
+  } else {
+    $('#signUpButton').append('<a class="btn btn-danger">Sorry! Our league is full for this seaon.</a>');
+  };
+}
+
 function populateTeamTable(team) {
   var percent = (parseInt(team.wins)/(parseInt(team.wins)+parseInt(team.losses))).toFixed(3);
 
@@ -131,6 +143,44 @@ function doPopovers() {
     track("Popover working!")
   });
 }
+
+function populateGameSchedules(x) {
+
+$('#gameSchedules').append(x);
+  if (x === 4) {
+    $('#gameSchedules').append(sched4);
+  } else if (x === 5 || x === 6) {
+    $('#gameSchedules').append(sched6);
+  } else if (x === 7 || x === 8) {
+    $('#gameSchedules').append(sched8);
+  } else {
+    $('#gameSchedules').append('<p class="text-error">There are not enough teams to start season play<p>');
+  }
+}
+
+var sched4 = [ 
+[ [1, 4], [2, 3] ],
+[ [1, 3], [2, 4] ],
+[ [1, 2], [3, 4] ]
+];
+
+var sched6 = [ 
+[ [1, 6], [2, 5], [3, 4] ],
+[ [1, 5], [4, 6], [2, 3] ],
+[ [1, 4], [3, 5], [2, 6] ],
+[ [1, 3], [2, 4], [5, 6] ],
+[ [1, 2], [3, 6], [4, 5] ]
+];
+
+var sched8 = [
+[ [1, 8], [2, 7], [3, 6], [4, 5] ],
+[ [1, 7], [6, 8], [2, 5], [3, 4] ],
+[ [1, 6], [5, 7], [4, 8], [2, 3] ],
+[ [1, 5], [4, 6], [3, 7], [2, 8] ],
+[ [1, 4], [3, 5], [2, 6], [7, 8] ],
+[ [1, 3], [2, 4], [5, 8], [6, 7] ],
+[ [1, 2], [3, 8], [4, 7], [5, 6] ]
+];
 
 // // reusable sort functions, and sort by any field
 // // http://stackoverflow.com/questions/979256/how-to-sort-an-array-of-javascript-objects
