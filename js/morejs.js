987c84596b9b5d355918704811dd2b016dfdbd12
Joe Shepherd <joeshepmedia@gmail.com>
2013-03-01 12:00:36 -0600

    Changed inline click events to JQuery, added delete team button, updated form graphics, inserted tooltips trigger for validation, validation still needs work to override submit

--- js/morejs.js
+++ js/morejs.js
@@ -1,35 +1,19 @@
 $(document).ready(function () {
-  track("Document Ready");
+  track("<i class='icon-file'></i> Document Ready");
   getFromDatabase();  
-}); // end ready
+  // $('form').form();
+   $("input").tooltip();
 
-function getFromDatabase() {  
+   $(".clear").click(function(){
+    clearForm()
+  });
 
-  $('#teamList').empty();
-  $('#teamTable tbody').empty();
-  $('#signUpButton').empty();
-  $('#gameSchedules').empty();
-  track("Cleared old data")
+$("#login").click(function() {
+  track("<i class='icon-wrench'></i> You are now logged in");
+  $(".manage").css("display", "inline");
+});
 
-  $.ajax({
-    url: 'backliftapp/team',
-    type: "GET",
-    dataType: "json",
-    success: function (data) {
-      track("Connected to database");
-      displayButton(data.length);
-      populateGameSchedules(data.length);
-      for (var i = 0; i < data.length; i++) {
-        populateTeamTable(data[i]);
-        populateTeamList(data[i]);
-      }
-      doPopovers();
-      track("Fresh data loaded");
-    }
-  }); // end ajax
-};
-
-function addTeam() {
+$("#addTeam").click(function(){
   var team = {
     name: $("#inputTeamName").val(),
     mgrFirst: $("#inputMgrFirst").val(),
@@ -47,13 +31,55 @@ function addTeam() {
     dataType: "json",
     data: team,
     success: function (data) {
-      console.dir(data);
-      track("Team: " + team.name + " added!");
+      track("<i class='icon-plus'></i> Team: " + team.name + " added!");
       clearForm();
       getFromDatabase();
     }
   }); // end ajax
-}; // end add team
+}); //end click
+
+}); // end ready
+
+function getFromDatabase() {  
+
+  $('#teamList').empty();
+  $('#teamTable tbody').empty();
+  $('#signUpButton').empty();
+  $('#gameSchedules').empty();
+  track("<i class='icon-trash'></i> Cleared old data")
+
+  $.ajax({
+    url: 'backliftapp/team',
+    type: "GET",
+    dataType: "json",
+    success: function (data) {
+      track("<i class='icon-hdd'></i> Connected to database");
+      track("<i class='icon-download-alt'></i> Fresh data loaded");
+      
+      // add scheduleId and wpc to records
+      for (var i = 0; i < data.length; i++) {
+        data[i]['scheduleId'] = i + 1;
+        data[i]['wpc'] = (parseInt(data[i].wins)/(parseInt(data[i].wins)+parseInt(data[i].losses))).toFixed(3);
+      }
+
+      // Teams Playing This Year
+      populateTeamList(data);
+      displayButton(data.length);
+
+      // League Standings      
+      data.sort(sort_by('wpc', true, parseFloat));
+      track("<i class='icon-random'></i> Teams sorted");
+      populateTeamTable(data);
+    
+      // Game Schedules
+      data.sort(sort_by('scheduleId', false, parseFloat));
+      populateGameSchedules(data);
+
+      doPopovers();
+    
+    }
+  }); // end ajax
+};
 
 function deleteTeam(id) {
   var conf = confirm("Are you sure you want to delete this team?");
@@ -62,49 +88,42 @@ function deleteTeam(id) {
       url: "backliftapp/team/" + id,
       type: "DELETE",
       dataType: "json",
+      success: function() { 
+        track("<i class='icon-minus'></i> Deleted: " + id);
+        getFromDatabase();
+      }
     });
-  track("deleted: " + id);
-  getFromDatabase();
   }
-}
+};
 
 function track(item) {
   $('#console').append(item + "<br>");
-}
-
-function clearForm() {
-  $(".teamImput").each(function () {
-    $(this).val("");
-  });
 };
 
-function manage() {
-  track("You are now logged in");
-  $(".manage").css("display", "inline");
-}
+function clearForm(){
+    $('#signupForm').each (function(){  
+    this.reset();
+   }); 
+  };
 
 function startSeason() {
   $(".playing").css("display", "none");
   $(".standings").css("display", "inline");
-  track("Season Started!")
-}
+  track("<i class='icon-warning-sign'></i>  Season Started!")
+};
 
 function populateTeamList(team) {
-  $(
-    "<h4 class='show'>" + team.name + " <em>sponsored by</em> " + team.sponsor + " </h4>" + 
-    "<p class='more'>" +
-    "Manager: " + team.mgrFirst + " " + team.mgrLast + "<br>" +
-    "Phone: " + team.mgrPhone + "<br>" +
-    "E-mail: " + team.mgrEmail + "<br>" +
-    "<a class='manage' onclick='deleteTeam(\"" + team.id + "\")'>Delete Team</a>" +
-    "</p>").appendTo('#teamList');
-}
-
-function displayButton() {
-  $(
-    document.write('<a href="#myModal" role="button" class="btn" data-toggle="modal">Sign up your team today!</a>')
-    ).appendTo('#signUpButton');
-}
+  for (var i = 0; i < team.length; i++) {
+    $(
+      "<h4 class='show'>" + team[i].name + " <em>sponsored by</em> " + team[i].sponsor + " </h4>" + 
+      "<p class='more'>" +
+      "Manager: " + team[i].mgrFirst + " " + team[i].mgrLast + "<br>" +
+      "Phone: " + team[i].mgrPhone + "<br>" +
+      "E-mail: " + team[i].mgrEmail + "<br>" +
+      "<button class='btn btn-mini manage' id='delete_me' type='button' onclick='deleteTeam(\"" + team[i].id + "\")'>Delete Team</button>" +
+      "</p>").appendTo('#teamList');
+  }
+};
 
 function displayButton(x) {
   if (x < 4) {
@@ -113,58 +132,105 @@ function displayButton(x) {
     $('#signUpButton').append('<a href="#myModal" role="button" class="btn" data-toggle="modal">Sign up your team today!</a>');
   } else {
     $('#signUpButton').append('<a class="btn btn-danger">Sorry! Our league is full for this seaon.</a>');
-  };
-}
+  }
+};
 
 function populateTeamTable(team) {
-  var percent = (parseInt(team.wins)/(parseInt(team.wins)+parseInt(team.losses))).toFixed(3);
+  for (var i = 0; i < team.length; i++) {
+    $(
+      "<tr>" +
+      "<td>" + (i+1) + "</td>" +
+      "<td><span class='show'>" + team[i].name + " <em>sponsored by</em> " + team[i].sponsor + "</span>" +
+      "<p class='more'><br>" +
+      "Manager: " + team[i].mgrFirst + " " + team[i].mgrLast + "<br>" +
+      "Phone: " + team[i].mgrPhone + "<br>" +
+      "E-mail: " + team[i].mgrEmail + "<br></p>" +
+      "</td>" +
+      "<td>" + team[i].wins + "</td>" +
+      "<td>" + team[i].losses + "</td>" +
+      "<td>" + team[i].wpc + "</td>" +
+      "</tr>").appendTo('#teamTable tbody');
+  }
+};
 
-  $(
-    "<tr>" +
-    "<td>" + team.id + "</td>" +
-    "<td><span class='show'>" + team.name + " <em>sponsored by</em> " + team.sponsor + "</span>" +
-    "<p class='more'><br>" +
-    "Manager: " + team.mgrFirst + " " + team.mgrLast + "<br>" +
-    "Phone: " + team.mgrPhone + "<br>" +
-    "E-mail: " + team.mgrEmail + "<br></p>" +
-    "</td>" +
-    "<td>" + team.wins + "</td>" +
-    "<td>" + team.losses + "</td>" +
-    "<td>" + percent + "</td>" +
-    "</tr>").appendTo('#teamTable tbody');
-}
-
-// Popover like functionality 
 function doPopovers() { 
   $('.more').hide();
   $('.show').click(
   function () {
     $(this).next('.more').toggle();
-    track("Popover working!")
+    track("<i class='icon-resize-vertical'></i> Popover working!")
   });
-}
+};
 
-function populateGameSchedules(x) {
+function populateGameSchedules(t) {
 
-$('#gameSchedules').append(x);
-  if (x === 4) {
-    $('#gameSchedules').append(sched4);
-  } else if (x === 5 || x === 6) {
-    $('#gameSchedules').append(sched6);
-  } else if (x === 7 || x === 8) {
-    $('#gameSchedules').append(sched8);
+  if (t.length === 4) {
+    var s = blankSchedule4;
+  } else if (t.length === 5 || t.length === 6) {
+    var s = blankSchedule6;
+  } else if (t.length === 7 || t.length === 8) {
+    var s = blankSchedule8;
   } else {
-    $('#gameSchedules').append('<p class="text-error">There are not enough teams to start season play<p>');
-  }
-}
+    $('<p class="text-error">There is not a schedule for the current amount of teams<p>').appendTo('#gameSchedules');
+    return; // dump from function
+  };
 
-var sched4 = [ 
+  if (t.length % 2 === 1) {
+    var oe = "Odd";
+    var x = 1;
+    var y = 0;
+    var z = 2;
+  } else {
+    var oe = "Even";
+    var x = 0;
+    var y = 1;
+    var z = 1;
+  };
+
+  // t-team s-schedule w-week g-game 0/1-Home/Away x,y,x,oe-variables to make odd schedules work
+  for (var w = 0; w < s.length; w++) {
+    $("<table class='table table-striped'><thead><tr>" +
+      "<th>Week " + (w + 1) + "</th>" +
+      "<th>Matchup</th>" +
+      "<th>Score</th>" +
+      "</tr></thead><tbody>" +
+        // will be populated by next for loop
+      "</tbody></table>").appendTo('#gameSchedules');
+    for (var g = x; g < s[w].length; g++) {
+      $("<tr>" +
+        "<td>" + (g + y) + ":00 pm" + "</td>" +
+        "<td>" + t[(s[w][g][0] - z)].name + " vs. " + t[(s[w][g][1] - z)].name + "</td>" +
+        "<td>" + "0-0 " + "<a class='manage'>Edit<span>" + "</a>" +
+        "</tr>").appendTo('tbody:last');
+    };
+    if (t.length % 2 === 1){
+      $("<tr class='warning'>" +
+        "<td>&nbsp;</td>" +
+        "<td>Team off: " + t[(s[w][0][1] - z)].name + "</td>" +
+        "<td>&nbsp;</td>" +
+        "<tr>").appendTo('tbody:last'); 
+    };
+  };
+  track("<i class='icon-calendar'></i>&nbsp;" + oe + " Schedule Loaded");
+
+}
+  
+  // // The raw Populate game schedule Magic -- Props to dmoore5050
+  // // t-team s-schedule w-week g-game 0/1-Home/Away
+  // for (var w = 0; w < s.length; w++) {
+  //   $("Week " + (w+1) + " Schedule<br>").appendTo('#gameSchedules');
+  //   for (var g = 0; g < s[w].length; g++) {
+  //     $(  t[s[w][g][0]].name + " vs. " + t[s[w][g][1]].name + "<br>").appendTo('#gameSchedules');
+  //   };
+  // };
+
+var blankSchedule4 = [ 
 [ [1, 4], [2, 3] ],
 [ [1, 3], [2, 4] ],
 [ [1, 2], [3, 4] ]
 ];
 
-var sched6 = [ 
+var blankSchedule6 = [ 
 [ [1, 6], [2, 5], [3, 4] ],
 [ [1, 5], [4, 6], [2, 3] ],
 [ [1, 4], [3, 5], [2, 6] ],
@@ -172,7 +238,7 @@ var sched6 = [
 [ [1, 2], [3, 6], [4, 5] ]
 ];
 
-var sched8 = [
+var blankSchedule8 = [
 [ [1, 8], [2, 7], [3, 6], [4, 5] ],
 [ [1, 7], [6, 8], [2, 5], [3, 4] ],
 [ [1, 6], [5, 7], [4, 8], [2, 3] ],
@@ -182,17 +248,18 @@ var sched8 = [
 [ [1, 2], [3, 8], [4, 7], [5, 6] ]
 ];
 
-// // reusable sort functions, and sort by any field
-// // http://stackoverflow.com/questions/979256/how-to-sort-an-array-of-javascript-objects
-// var sort_by = function (field, reverse, primer) {
-//     var key = primer ? function (x) {
-//             return primer(x[field]);
-//         } : function (x) {
-//             return x[field];
-//         }
-//     return function (a, b) {
-//         var A = key(a),
-//             B = key(b);
-//         return (A < B ? -1 : (A > B ? 1 : 0)) * [1, -1][+ !! reverse];
-//     }
-// }
+// reusable sort functions, and sort by any field
+// http://stackoverflow.com/questions/979256/how-to-sort-an-array-of-javascript-objects
+var sort_by = function (field, reverse, primer) {
+    var key = primer ? function (x) {
+            return primer(x[field]);
+        } : function (x) {
+            return x[field];
+        }
+    return function (a, b) {
+        var A = key(a),
+            B = key(b);
+        return (A < B ? -1 : (A > B ? 1 : 0)) * [1, -1][+ !! reverse];
+    }
+}
+
